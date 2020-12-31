75f621ae940418f1afc93c87715dd763e2db9067
arctronic <shihabsikder98@gmail.com>
2020-12-31 11:36:52 +0600

    Scrollabe login and reg page

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -4,7 +4,6 @@ import 'package:lifeline/constants.dart';
 import 'package:modal_progress_hud/modal_progress_hud.dart';
 import 'package:lifeline/services/authenticate.dart';
 
-
 class LoginScreen extends StatefulWidget {
   static String id = 'login';
 
@@ -22,69 +21,72 @@ class _LoginScreenState extends State<LoginScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: ModalProgressHUD(
-        color: Colors.white,
-        opacity: 0.9,
-        progressIndicator: kWaveLoadingIndicator,
-        inAsyncCall: loadingIndicator,
-        child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 24.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Hero(
-                tag: 'logo',
-                child: Container(
-                  height: 300.0,
-                  child: Image.asset(
-                    'assets/images/lifeline_logo.png',
+      body: SingleChildScrollView(
+        child: ModalProgressHUD(
+          color: Colors.white,
+          opacity: 0.9,
+          progressIndicator: kWaveLoadingIndicator,
+          inAsyncCall: loadingIndicator,
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 24.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Hero(
+                  tag: 'logo',
+                  child: Container(
+                    height: 300.0,
+                    child: Image.asset(
+                      'assets/images/lifeline_logo.png',
+                    ),
                   ),
                 ),
-              ),
-              SizedBox(
-                height: 48.0,
-              ),
-              TextField(
-                keyboardType: TextInputType.emailAddress,
-                onChanged: (value) {
-                  this.email = value;
-                },
-                decoration: kTextFieldDecoration.copyWith(hintText: "Email"),
-              ),
-              SizedBox(
-                height: 8.0,
-              ),
-              TextField(
-                obscureText: true,
-                onChanged: (value) {
-                  this.password = value;
-                },
-                decoration: kTextFieldDecoration.copyWith(hintText: "Password"),
-              ),
-              SizedBox(
-                height: 24.0,
-              ),
-              RoundedButton(
-                text: 'Log In',
-                color: Colors.green[900],
-                onPressed: () async {
-                  setState(() {
-                    loadingIndicator = true;
-                  });
-                  try {
-                    // TODO: Implement login functionalities
-                    Auth().signIn(this.email, this.password);
-                    print(Auth().getUID());
+                SizedBox(
+                  height: 48.0,
+                ),
+                TextField(
+                  keyboardType: TextInputType.emailAddress,
+                  onChanged: (value) {
+                    this.email = value;
+                  },
+                  decoration: kTextFieldDecoration.copyWith(hintText: "Email"),
+                ),
+                SizedBox(
+                  height: 8.0,
+                ),
+                TextField(
+                  obscureText: true,
+                  onChanged: (value) {
+                    this.password = value;
+                  },
+                  decoration:
+                      kTextFieldDecoration.copyWith(hintText: "Password"),
+                ),
+                SizedBox(
+                  height: 24.0,
+                ),
+                RoundedButton(
+                  text: 'Log In',
+                  color: Colors.green[900],
+                  onPressed: () async {
                     setState(() {
-                      loadingIndicator = false;
+                      loadingIndicator = true;
                     });
-                  } catch (e) {
-                    print(e);
-                  }
-                },
-              ),
-            ],
+                    try {
+                      // TODO: Implement login functionalities
+                      Auth().signIn(this.email, this.password);
+                      print(Auth().getUID());
+                      setState(() {
+                        loadingIndicator = false;
+                      });
+                    } catch (e) {
+                      print(e);
+                    }
+                  },
+                ),
+              ],
+            ),
           ),
         ),
       ),
