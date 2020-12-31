e5e14c5b0192f9a10a294e23c8a891f2722569b3
zedrex <adib14021999@gmail.com>
2020-12-31 20:10:18 +0600

    Added exception error messages

--- lib/services/authenticate.dart
+++ lib/services/authenticate.dart
@@ -60,4 +60,41 @@ class Auth {
       return null;
     }
   }
+
+  static String getError(String exception) {
+    switch (exception) {
+      case "ERROR_EMAIL_ALREADY_IN_USE":
+      case "account-exists-with-different-credential":
+      case "email-already-in-use":
+        return "Email already used. Go to login page.";
+        break;
+      case "ERROR_WRONG_PASSWORD":
+      case "wrong-password":
+        return "Wrong email/password combination.";
+        break;
+      case "ERROR_USER_NOT_FOUND":
+      case "user-not-found":
+        return "No user found with this email.";
+        break;
+      case "ERROR_USER_DISABLED":
+      case "user-disabled":
+        return "User disabled.";
+        break;
+      case "ERROR_TOO_MANY_REQUESTS":
+      case "operation-not-allowed":
+        return "Too many requests to log into this account.";
+        break;
+      case "ERROR_OPERATION_NOT_ALLOWED":
+      case "operation-not-allowed":
+        return "Server error, please try again later.";
+        break;
+      case "ERROR_INVALID_EMAIL":
+      case "invalid-email":
+        return "Email address is invalid.";
+        break;
+      default:
+        return "Login failed. Please try again.";
+        break;
+    }
+  }
 }
