2b12ca71b8d1950a7856680bd4658462d6729a15
zedrex <adib14021999@gmail.com>
2021-01-07 09:16:59 +0600

    Added Icon button to call from Donor list directly

--- lib/components/donor_info_card.dart
+++ lib/components/donor_info_card.dart
@@ -3,11 +3,26 @@ import 'package:flutter/services.dart';
 import 'package:lifeline/constants.dart';
 import 'package:lifeline/models/blood_donor.dart';
 import 'package:toast/toast.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class DonorInfoCard extends StatelessWidget {
   final Donor donor;
 
   DonorInfoCard({this.donor});
+
+  callNumber(BuildContext context, String number) async {
+    var url = 'tel:$number';
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      Toast.show(
+        'Cannot launch Phone',
+        context,
+        duration: Toast.LENGTH_SHORT,
+        gravity: Toast.BOTTOM,
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -40,36 +55,59 @@ class DonorInfoCard extends StatelessWidget {
             ),
           ],
         ),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Text(
-              this.donor.blood,
-              style: kTextStyle.copyWith(
-                fontSize: 56,
-                color: Colors.green[900],
-                fontWeight: FontWeight.bold,
+        child: Row(
+          children: [
+            Expanded(
+              flex: 3,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  Text(
+                    this.donor.blood,
+                    style: kTextStyle.copyWith(
+                      fontSize: 56,
+                      color: Colors.green[900],
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    this.donor.name,
+                    style: kTextStyle.copyWith(
+                      fontSize: 18,
+                    ),
+                  ),
+                  Text(
+                    this.donor.contact,
+                    style: kTextStyle.copyWith(
+                      fontSize: 18,
+                    ),
+                  ),
+                  Text(
+                    this.donor.location,
+                    style: kTextStyle.copyWith(
+                      fontSize: 18,
+                    ),
+                  ),
+                ],
               ),
             ),
-            Text(
-              this.donor.name,
-              style: kTextStyle.copyWith(
-                fontSize: 18,
+            Expanded(
+              flex: 1,
+              child: IconButton(
+                onPressed: () {
+                  callNumber(
+                    context,
+                    this.donor.contact,
+                  );
+                },
+                icon: Icon(
+                  Icons.phone,
+                  size: 30,
+                  color: Colors.green[900],
+                ),
               ),
-            ),
-            Text(
-              this.donor.contact,
-              style: kTextStyle.copyWith(
-                fontSize: 18,
-              ),
-            ),
-            Text(
-              this.donor.location,
-              style: kTextStyle.copyWith(
-                fontSize: 18,
-              ),
-            ),
+            )
           ],
         ),
       ),
